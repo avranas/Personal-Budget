cc2e81ff9e223ba2333291260420ab7b7111789c
Alex Vranas <62129976+avranas@users.noreply.github.com>
2022-06-27 13:43:34 -0700

    Add files via upload

--- db-config.js
+++ db-config.js
@@ -1,10 +1,17 @@
 const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'alex',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'password',
-  port: 5432,
-});
+// const pool = new Pool({
+//   user: 'alex',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'password',
+//   port: 5432,
+// });
 
+const pool = new Pool({
+   connectionString: process.env.DATABASE_URL,
+   ssl: {
+     rejectUnauthorized: false
+   }
+ });
+ 
 module.exports = {pool};
